59239b56175d071750ca28739f67fd3ba8e4ceaa
Alice <coisne.alice@gmail.com>
2021-02-19 09:13:05 +0100

    ajout commentaire code auth.js + sauce_routes

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,16 +3,16 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1]; //récupère le 2eme élement du tableau 
-    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    const userId = decodedToken.userId;
-    if (req.body.userId && req.body.userId !== userId) {
-      throw 'Invalid user ID';
+    const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET'); //décode le token, verify (le token) + cl" secrete pour la créa du token
+    const userId = decodedToken.userId; //récurérer le token du decoded token
+    if (req.body.userId && req.body.userId !== userId) { //si userId ne correspond pas au token
+      throw 'UserId non valable'; //renvoi l'erreur 
     } else {
-      next(); //si tout va bien, étape suivante au prochain middleware
+      next(); //si tout va bien, passer la requete au prochain middleware
     }
   } catch {
     res.status(401).json({
-      error: new Error('Invalid request!')
+      error: new Error('Requete invalide')
     });
   }
 };
