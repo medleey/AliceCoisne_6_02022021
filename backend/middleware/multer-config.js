e9ed9f6ff9f61fc3316ddf826ce693efa7185bdd
Alice <coisne.alice@gmail.com>
2021-02-19 09:11:36 +0100

    ajout commentaire code (multer-config)

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,20 +1,20 @@
-const multer = require('multer');
+const multer = require('multer'); //importation de multer
 
-const MIME_TYPES = {
+const MIME_TYPES = { //génère l'extension du fichier
   'image/jpg': 'jpg',
   'image/jpeg': 'jpg',
   'image/png': 'png'
 };
 
-const storage = multer.diskStorage({
-  destination: (req, file, callback) => {
-    callback(null, 'images'); //null= pas d'erreur à cet endroit 
+const storage = multer.diskStorage({            //enregistret sur le disque
+  destination: (req, file, callback) => {       //fonction qui va retourner à multer ou save le fichier 
+    callback(null, 'images');                   //null= pas d'erreur à cet endroit, 2e arg : dossier de destination
   },
-  filename: (req, file, callback) => { // explique à multer quel nom de fichier utilisé 
-    const name = file.originalname.split(' ').join('_');
-    const extension = MIME_TYPES[file.mimetype];
-    callback(null, name.split('.')[0] + '_' + Date.now() + '.' + extension);
+  filename: (req, file, callback) => {          // explique à multer quel nom de fichier utiliser
+    const name = file.originalname.split(' ').join('_'); //génère le nouveau nom du fichier 
+    const extension = MIME_TYPES[file.mimetype];        //élément du dico qui correspond au mine-type du fichier envoyé par le front
+    callback(null, name.split('.')[0] + '_' + Date.now() + '.' + extension); //date.now = ajoute un time stamp pour le rendre le plus unique possible
   }
 });
 
-module.exports = multer({storage: storage}).single('image'); //fichier unique et non pas grupe de fichier 
+module.exports = multer({storage: storage}).single('image'); //fichier unique (et non pas grupe de fichier), fichier image uniquement 
